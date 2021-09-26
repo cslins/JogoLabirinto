7bc5b2d7cfd51f1d6af3117abbe80c98e6d81c37
Gabriel Aragão <31046857+linknasubi@users.noreply.github.com>
2021-09-26 17:45:24 -0300

    Added counter and updated some graphical stuff

--- Caminhos.py
+++ Caminhos.py
@@ -1,148 +1,127 @@
+from types import LambdaType
 from Graph_Lab import *
-import random
 
 
-class Caminhos:
+class Cam:
+    def __init__(self, lab: Labirinto, n) -> None:
 
-    def __init__(self, lab: Labirinto, n: int = 1) -> None:
         self.lab = lab
-        self.cam = []
+        self.n = n
+
         self.vizit = []
 
-        for i in range(self.lab.i):
-            linha_cam = []
-            for j in range(self.lab.j):
-                linha_cam.append([])
-
-            self.cam.append(linha_cam)
-
-        for w in range(1, n+1):
-            self.caminho_ini(w)
-
-        
-    def caminho_ini(self, num):
-        self.vizit = []
         for i in range(self.lab.i):
             linha_vizit = []
             for j in range(self.lab.j):
                 linha_vizit.append(False)
 
             self.vizit.append(linha_vizit)
+
         
+    def define_caminho(self):
 
-        pilha = [self.lab.inicio]
-
-        while pilha[-1] != self.lab.fim:
-            self.vizit[pilha[-1].i][pilha[-1].j] = True
-            opc = self.vizinhos_validos(pilha[-1])
-
-            if opc == []:
-                pilha.pop()
-            else:
-                pilha.append(random.choice(opc))
-        
-        print()
-        desvio = -1
-        for n in range(len(pilha) - 1):
-            i = pilha[n].i
-            j = pilha[n].j
-
-            if desvio == -1:
-                if self.cam[i][j] == []:
-                    desvio = n
-
-            else:
-                if self.cam[i][j] != []:
-                    pilha = pilha[desvio-1:n+1]
-                    break
-
-
-        for n in range(len(pilha) - 1):
-            pilha[n].add_vizinho(pilha[n+1])
-            i = pilha[n].i
-            j = pilha[n].j
-
-            self.cam[i][j].append(num)
-
-        self.cam[pilha[-1].i][pilha[-1].j].append(num)
-
-        for it in pilha:
-            print(it.i, it.j)
-
-
-
-    def vizinhos_validos(self, v: Vertice) -> list:
-        opcoes = []
-
-        i = v.i
-        j = v.j
-
-        if i > 0:
-            if not self.vizit[i-1][j]:
-                opcoes.append(self.lab.matriz[i-1][j])
-
-        if i < self.lab.i - 1:
-            if not self.vizit[i+1][j]:
-                opcoes.append(self.lab.matriz[i+1][j])
-
-        if j > 0:
-            if not self.vizit[i][j-1]:
-                opcoes.append(self.lab.matriz[i][j-1])
-
-        if j < self.lab.j - 1:
-            if not self.vizit[i][j+1]:
-                opcoes.append(self.lab.matriz[i][j+1])
-
-
-        return opcoes
-
-    def semsaida(self):
+        disponiveis = []
         for i in range(self.lab.i):
             for j in range(self.lab.j):
-                if self.cam[i][j] != []:
-                    self.acessar(self.lab.matriz[i][j])
-    
-    def acessar(self, v: Vertice):
+                disponiveis.append(self.lab.matriz[i][j])
 
-        i = v.i
-        j = v.j
+        self.vizit[self.lab.fim.i][self.lab.fim.j] = True
 
-        pilha = [v]
+        self.caminho(self.lab.inicio, 0, self.lab.i - 1, 0, self.lab.j - 1)
 
-        while pilha != []:
+        for num in range(1, self.n):
+            vert = random.choice(disponiveis)
+            while self.vizit[vert.i][vert.j]:
+                disponiveis.remove(vert)
+                if disponiveis != []:
+                    vert = random.choice(disponiveis)
+                else:
+                    return
+            print(vert.i, vert.j)
+
+            if vert.i == 0:
+                i2 = vert.i
+                if vert.j < self.lab.inicio.j:
+                    j1 = vert.j + 1
+                    j2 = self.lab.j - 1
+                else:
+                    j1 = 0
+                    j2 = vert.j - 1
+            else:
+                i2 = vert.i - 1
+                j1 = 0
+                j2 = self.lab.j - 1
+
+            
+            self.caminho(vert, 0, i2, j1, j2)
+
+            if vert.i == self.lab.i - 1:
+                i1 = vert.i
+                if vert.j < self.lab.fim.j:
+                    j1 = vert.j + 1
+                    j2 = self.lab.j - 1
+                else:
+                    j1 = 0
+                    j2 = vert.j - 1
+            else:
+                i1 = vert.i + 1
+                j1 = 0
+                j2 = self.lab.j - 1
+
+            self.caminho(vert, i1, self.lab.i - 1, j1, j2)
+
+        for vrt in disponiveis:
+            if not self.vizit[vrt.i][vrt.j]:
+                self.caminho(vrt,0, self.lab.i - 1, 0, self.lab.j - 1)
+ 
+
+    def caminho(self, origem: Vertice, i1, i2, j1, j2):
+
+        acess = []
+        for i in range(self.lab.i):
+            linha_acess = []
+            for j in range(self.lab.j):
+                linha_acess.append(False)
+
+            acess.append(linha_acess)
+
+        self.vizit[origem.i][origem.j] = False
+        
+        pilha = [origem]
+
+        while not self.vizit[pilha[-1].i][pilha[-1].j]:
+
+            acess[pilha[-1].i][pilha[-1].j] = True
+
             opcoes = []
 
             i = pilha[-1].i
             j = pilha[-1].j
 
-            
-            self.cam[i][j] = [0]
-
-            if i > 0:
-                if self.cam[i-1][j] == []:
+            if i > i1 and j1<=j<=j2:
+                if not acess[i-1][j]:
                     opcoes.append(self.lab.matriz[i-1][j])
 
-            if i < self.lab.i - 1:
-                if self.cam[i+1][j] == []:
+            if i < i2 and j1<=j<=j2:
+                if not acess[i+1][j]:
                     opcoes.append(self.lab.matriz[i+1][j])
 
-            if j > 0:
-                if self.cam[i][j-1] == []:
+            if j > j1 and i1<=i<=i2:
+                if not acess[i][j-1]:
                     opcoes.append(self.lab.matriz[i][j-1])
 
-            if j < self.lab.j - 1:
-                if self.cam[i][j+1] == []:
+            if j < j2 and i1<=i<=i2:
+                if not acess[i][j+1]:
                     opcoes.append(self.lab.matriz[i][j+1])
 
-            if opcoes != []:
-                escolha = random.choice(opcoes)
-
-                pilha[-1].add_vizinho(escolha)
-
-                pilha.append(escolha)
-            else:
+            if opcoes == []:
                 pilha.pop()
-                
+            else:
+                pilha.append(random.choice(opcoes))
+ 
+        for n in range(len(pilha) - 1):
+            pilha[n].add_vizinho(pilha[n+1])
 
-
-
+        for v in pilha:
+            self.vizit[v.i][v.j] = True
+            #print(v.i, v.j)
--- Game.py
+++ Game.py
@@ -4,26 +4,30 @@ from Caminhos import *
 
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 
-window_width = 1080
+window_width = 1280
 window_height = 720
 
+labi_width = window_width * 0.8
+labi_height = labi_width * (9/16)
 
-a = 3
-l = 3
+
+a = 30
+l = 30
 
 
 class Map:
     
     def __init__(self, height:int, width:int, n_paths:int):
         
-        self.labirin = Labirinto(height, width)
-        self.Path = Caminhos(self.labirin, 1)
+        self.Labirin = Labirinto(height, width)
+        self.Path = Cam(self.Labirin, 1)
+        self.Path.define_caminho()
         self.height = height
         self.width = width
         
-        self.graph_map = self.labirin.matriz
+        self.graph_map = self.Labirin.matriz
         self.matrix_map = []
-        self.labirin.imprimir()
+        self.Labirin.imprimir()
 
         
         for i in range(height):
@@ -43,7 +47,7 @@ class Map:
             print()
             for j in range(self.width):
                 print(self.graph_map[i][j].id, end='   ')
-
+                
 
 matrix_map = Map(a, l, 1)
 
@@ -51,40 +55,84 @@ matrix_map = Map(a, l, 1)
 pygame.init()
 window = pygame.display.set_mode((window_width,window_height))
 clock = pygame.time.Clock()
-
-x = 120
-y = 120
 running = True
+
+
 
 
 
 def drawStroke(actual_i, actual_j):
     
-    borderPixels = 20
+    borderPixels = int(((labi_width)/matrix_map.width) * 0.2)
     
-    initialPosX = (window_width * actual_j)/matrix_map.width
-    initialPosY = (window_height * actual_i)/matrix_map.height
+
+    initialPosX = (labi_width * actual_j )/matrix_map.width + 0.10*window_width
+    initialPosY = (labi_height * actual_i)/matrix_map.height + 0.10*window_height
     
-    newPosX = initialPosX + (window_width/matrix_map.width)
-    newPosY = initialPosY + (window_height/matrix_map.height)
+    newPosX = initialPosX + (labi_width/matrix_map.width)
+    newPosY = initialPosY + (labi_height/matrix_map.height)
     
     
-    if not (matrix_map.graph_map[i][j].acima()):
+    if not (matrix_map.graph_map[actual_i][actual_j].acima()):
         pygame.draw.line(window,(0,0,255),(initialPosX, initialPosY), (newPosX, initialPosY), borderPixels)
     
     
-    if not (matrix_map.graph_map[i][j].abaixo()):
+    if not (matrix_map.graph_map[actual_i][actual_j].abaixo()):
         pygame.draw.line(window,(0,0,255),(initialPosX, newPosY), (newPosX, newPosY), borderPixels)
         
         
-    if not (matrix_map.graph_map[i][j].direita()):
+    if not (matrix_map.graph_map[actual_i][actual_j].direita()):
         pygame.draw.line(window,(0,0,255),(newPosX, initialPosY), (newPosX, newPosY), borderPixels)
         
         
-    if not (matrix_map.graph_map[i][j].esquerda()):
+    if not (matrix_map.graph_map[actual_i][actual_j].esquerda()):
         pygame.draw.line(window,(0,0,255),(initialPosX, initialPosY), (initialPosX, newPosY), borderPixels)
-        
-        
+
+
+def movePlayer(actual_i, actual_j, playerX, playerY, stepsCounter):
+    
+    
+    if event.key == pygame.K_RIGHT and (matrix_map.graph_map[actual_i][actual_j].direita()):
+        playerX += (labi_width/matrix_map.width)
+        actual_j += 1
+        stepsCounter += 1
+    if event.key == pygame.K_LEFT and (matrix_map.graph_map[actual_i][actual_j].esquerda()):
+        playerX -= (labi_width/matrix_map.width)
+        actual_j -= 1
+        stepsCounter += 1
+    if event.key == pygame.K_DOWN and (matrix_map.graph_map[actual_i][actual_j].abaixo()):
+        playerY += (labi_height/matrix_map.height)
+        actual_i += 1
+        stepsCounter += 1
+    if event.key == pygame.K_UP and (matrix_map.graph_map[actual_i][actual_j].acima()):
+        playerY -= (labi_height/matrix_map.height)
+        actual_i -= 1
+        stepsCounter += 1
+    
+    print(playerX, playerY)
+    
+    return actual_i, actual_j, playerX, playerY, stepsCounter
+
+
+def drawCounter(value):
+    text = font.render(str(value), True, (0,0,0), (255, 255, 255))
+    textRect = text.get_rect()
+    textRect.center = (window_width*0.95, window_height*0.1)
+    window.blit(text, textRect)
+    
+
+actual_i= matrix_map.Labirin.inicio.i
+actual_j  = matrix_map.Labirin.inicio.j
+playerX = ((labi_width * actual_j)/matrix_map.width) + (window_width - labi_width) * 0.55
+playerY = ((labi_height * actual_i) /matrix_map.height) + (window_height - labi_height) * 0.55
+playerWidth = ((labi_width)/matrix_map.width) * 0.4
+playerHeight = ((labi_height)/matrix_map.height) *0.4
+stepsCounter = 0
+
+
+font = pygame.font.Font('freesansbold.ttf', 32)
+
+print(actual_i, actual_j)
 
 
 while running:
@@ -94,20 +142,17 @@ while running:
             running = False
             break
         elif event.type == pygame.KEYDOWN:
+            actual_i, actual_j, playerX, playerY, stepsCounter = movePlayer(actual_i, actual_j, playerX, playerY, stepsCounter)
             if event.key == pygame.K_ESCAPE:
                 running = False
                 break
-            elif event.key == pygame.K_RIGHT:
-                x += 8
-            elif event.key == pygame.K_LEFT:
-                x -= 8
-            elif event.key == pygame.K_DOWN:
-                y += 8
-            elif event.key == pygame.K_UP:
-                y -= 8
+
 
 
     window.fill((255,255,255))
+    
+    pygame.draw.rect(window,(173,255,47), pygame.Rect((playerX, playerY), (playerWidth, playerHeight)))
+    drawCounter(stepsCounter)
     
 
     for i in range(matrix_map.height):
@@ -123,4 +168,7 @@ while running:
 
     clock.tick(60)
 
+
 pygame.quit()
+
+
